e7cc45050d39dd3cc1d2658c73b5f839feba3e46
Shahjalal Hossain <shossainfolk@gmail.com>
2014-12-15 00:55:37 +0600

    This commit is for http://www.w3programmers.com/user-registration-with-symfony/

---
+++ symfony/src/Custom/BookBundle/Form/UserType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Custom\BookBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class UserType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username')
+            ->add('password', 'password')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Custom\BookBundle\Entity\User'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'custom_bookbundle_user';
+    }
+}
