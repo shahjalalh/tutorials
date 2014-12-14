dbcd984cae04d9f3990cbb23cbce87bb8b9f7dd4
Shahjalal Hossain <shossainfolk@gmail.com>
2014-12-14 11:39:41 +0600

    This commit is for my tutorial project on http://www.w3programmers.com/login-and-authentication-with-symfony/

---
+++ symfony/src/Custom/BookBundle/Controller/SecurityController.php
@@ -0,0 +1,35 @@
+<?php
+namespace Custom\BookBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\SecurityContextInterface;
+
+class SecurityController extends Controller
+{
+    public function loginAction(Request $request)
+    {
+        $session = $request->getSession();
+
+        // get the login error if there is one
+        if ($request->attributes->has(SecurityContextInterface::AUTHENTICATION_ERROR)) {
+            $error = $request->attributes->get(
+                SecurityContextInterface::AUTHENTICATION_ERROR
+            );
+        } elseif (null !== $session && $session->has(SecurityContextInterface::AUTHENTICATION_ERROR)) {
+            $error = $session->get(SecurityContextInterface::AUTHENTICATION_ERROR);
+            $session->remove(SecurityContextInterface::AUTHENTICATION_ERROR);
+        } else {
+            $error = '';
+        }
+
+
+        return $this->render(
+            'CustomBookBundle:Security:login.html.twig',
+            array(
+                'error'         => $error,
+            )
+        );
+    }
+}
+
